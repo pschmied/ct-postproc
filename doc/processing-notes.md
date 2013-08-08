585e9eaf5d51fe75ff9329f08d77b1f457d9dd17
Peter Schmiedeskamp <peter@thoughtspot.net>
2013-08-08 10:48:55 -0700

    initial commit

---
+++ doc/processing-notes.md
@@ -0,0 +1,76 @@
+# ETL
+ogr2ogr is my homeboy
+
+# Truck volumes on every roadway
+Use WSDOT FGTSWA data (FGTSclass attribute) to predict the truck
+counts observed in our trucks_counts layer. If this doesn't totally
+suck, then we make up numbers for remainder of network.
+
+Best bet: join i,j in truck counts table to i,j and j,i in edges table, run regression.
+
+recode NULL tonnage as 4
+counts ~ tonnage
+counts ~ tonnage + facilityInMIC?
+counts ~ streetclass + facilityInMIC?
+
+Problems: 
+ 1. multicollinearity with street classification (how to brain
+    this?) - check that street classification isn't a better predictor
+    or somethng.
+
+
+
+http://www.wsdot.wa.gov/mapsdata/geodatacatalog/Maps/noscale/DOT_Cartog/FGTSWA.htm
+
+# Identify left vs. right vs. non-turns
+Peng's lit review. Probably threshold of degrees. Peter can probably
+gin something up.
+
+# Filter GPS
+
+# Travel time
+From the traces data, as is, minus stopping times > 3 min
+
+# Travel distance
+Geometric, from traces
+
+# Travel speed
+Median GPS speed
+
+# Trip frequency
+From data, self reported, as is
+
+# Age
+From data, self reported, as is
+
+# Gender
+From data, self reported, as is
+
+# Number of collisions (trip level 200m)
+All ten years? 
+
+# Number of intersections (trip level 10m)
+
+# Number of right turns (trip level 10m)
+
+# Proportion of up-slope per trip
+
+# Mean upslope
+
+# Proportion of trips on cycling facilities
+
+# Density... (trip level 200m)
+
+# Land use mix (trip level 200m)
+
+# Number of grocery stores (trip level 200m)
+
+# Number of bus stops (trip level 200m)
+
+# Origin access to closest facility (100m)
+
+# Destination access to closest facility (100m)
+
+# Energy consumption
+
+# Speed limit (trip level)
---
+++ src/etl.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Extract transform and load procedures
+
+# Make for nominal portability to PostGIS
+ofmt="-f SQLite"
+basepath=~/Desktop/CycleTracks/data
+orig=$basepath/original
+opath=$basepath/cycletracks.sqlite
+flags="-t_srs EPSG:4326 -dsco SPATIALITE=YES -gt 65536"
+append=$flags" -append -update"
+
+rm $opath
+
+# Full PSRC roads network (TransRefEdges and TransRefJunctions)
+ogr2ogr $ofmt $opath $orig/PSRC_Net.gdb $flags
+
+# Collisions
+ogr2ogr $ofmt $opath $orig/CollisionsData/Merged/Collisions_Merged.shp \
+$append -nln collisions
+
+# Bicycle facilities - FAILS (need to bring up to speed in ArcCatalog)
+ogr2ogr $ofmt $opath $orig/bikefacility/bikefacilities.gdb $append
+
+# CycleTracks Traces
+ogr2ogr $ofmt $opath $orig/cycletracks/traces.csv $append -s_srs EPSG:4326
+spatialite $opath \
+"SELECT AddGeometryColumn('traces','Geometry',4326,'POINT',2)"
+spatialite $opath \
+"UPDATE traces SET Geometry=MakePoint(longitude,latitude, 4326);"
+
+# Grocery stores
+ogr2ogr $ofmt $opath \
+$orig/grocery/Snohomish_major_stores_clean_1_w_PIN_and_SF_FINAL.shp \
+$append -nln grocery_snohomish
+
+ogr2ogr $ofmt $opath $orig/grocery/KITSAP_FINAL_COMPLETE_20090401.shp \
+$append -nln grocery_kitsap
+
+ogr2ogr $ofmt $opath $orig/grocery/grocery_stores_20090311.shp \
+$append -nln grocery_king
+
+# Mode attributes - table only, fake input srs
+ogr2ogr $ofmt $opath $orig/NetAtts.gdb \
+$append -s_srs EPSG:4326 -nln network_attributes
+
+# Junction elevations
+ogr2ogr $ofmt $opath $orig/network/junction_elevation.gdb \
+$append -nln elevation_junctions
+
+# TransRefEdges with elevation
+ogr2ogr $ofmt $opath $orig/network/peters_transrefedges.gdb \
+$append -nln elevation_edges
+
+# Parcels; appending -skipfailures, but should validate we're not
+# losing much valid data, though looks like only one geometry. Slow as
+# fsck.
+ogr2ogr $ofmt $opath $orig/parcel/prcl05/prcl05.shp \
+$append -skipfailures -nln parcels_2005
+
+# Truck volumes - waiting on Alon's guidance
+ogr2ogr $ofmt $opath $orig/Truck_Volumes/combined_classification_counts.shp \
+$append -nln truck_counts
+
+# Truck freight functional classification crap
+ogr2ogr $ofmt $opath $orig/FGTSWA.gdb \
+$append -nln freight_classification
