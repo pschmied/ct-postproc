b81fee64f8243db8ba990bb02c2e756fb2a144bb
Peter Schmiedeskamp <peter@thoughtspot.net>
2013-08-14 10:42:15 -0700

    switch to projected web datum

--- src/etl.sh
+++ src/etl.sh
@@ -6,33 +6,39 @@ ofmt="-f SQLite"
 basepath=~/Desktop/ct-postproc/data
 orig=$basepath/original
 opath=$basepath/cycletracks.sqlite
-flags="-t_srs EPSG:4326 -dsco SPATIALITE=YES -gt 65536"
+flags="-t_srs EPSG:3857 -a_srs EPSG:3857 -dsco SPATIALITE=YES -gt 65536"
 append=$flags" -append -update"
 
 rm $opath
 
 # Full PSRC roads network (TransRefEdges and TransRefJunctions)
+echo "psrc network"
 ogr2ogr $ofmt $opath $orig/PSRC_Net.gdb $flags
 
 # Collisions
+echo "collisions"
 ogr2ogr $ofmt $opath $orig/CollisionsData/Merged/Collisions_Merged.shp \
-$append -nln collisions
+$append -nln collisions -skipfailures
 
 # Bicycle facilities - FAILS (need to bring up to speed in ArcCatalog)
+echo "bike facilities"
 ogr2ogr $ofmt $opath $orig/bikefacility/bikefacilities.gdb $append
 
 # CycleTracks Traces
-ogr2ogr $ofmt $opath $orig/cycletracks/traces.csv $append -s_srs EPSG:4326
+echo "traces"
+ogr2ogr $ofmt $opath $orig/cycletracks/traces.csv $append -s_srs EPSG:3857
 spatialite $opath \
-"SELECT AddGeometryColumn('traces','Geometry',4326,'POINT',2)"
+"SELECT AddGeometryColumn('traces','Geometry',3857,'POINT',2)"
 spatialite $opath \
-"UPDATE traces SET Geometry=MakePoint(longitude,latitude, 4326);"
+"UPDATE traces SET Geometry=MakePoint(longitude,latitude, 3857);"
 
 # CycleTracks Trips (fake SRS again)
+echo "trips"
 ogr2ogr $ofmt $opath \
-$orig/cycletracks/trips.csv $append -s_srs EPSG:4326
+$orig/cycletracks/trips.csv $append -s_srs EPSG:3857
 
 # Grocery stores
+echo "groceries"
 ogr2ogr $ofmt $opath \
 $orig/grocery/Snohomish_major_stores_clean_1_w_PIN_and_SF_FINAL.shp \
 $append -nln grocery_snohomish
@@ -44,31 +50,38 @@ ogr2ogr $ofmt $opath $orig/grocery/grocery_stores_20090311.shp \
 $append -nln grocery_king
 
 # Mode attributes - table only, fake input srs
+echo "mode attributes"
 ogr2ogr $ofmt $opath $orig/NetAtts.gdb \
-$append -s_srs EPSG:4326 -nln network_attributes
+$append -s_srs EPSG:3857 -nln network_attributes
 
 # Junction elevations
+echo "junction elevations"
 ogr2ogr $ofmt $opath $orig/network/junction_elevation.gdb \
 $append -nln elevation_junctions
 
 # TransRefEdges with elevation
+echo "edges elevations"
 ogr2ogr $ofmt $opath $orig/network/peters_transrefedges.gdb \
 $append -nln elevation_edges
 
 # Parcels; appending -skipfailures, but should validate we're not
 # losing much valid data, though looks like only one geometry. Slow as
 # fsck.
+echo "parcels"
 ogr2ogr $ofmt $opath $orig/parcel/prcl05/prcl05.shp \
 $append -skipfailures -nln parcels_2005
 
 # Truck volumes - waiting on Alon's guidance
+echo "truck volumes"
 ogr2ogr $ofmt $opath $orig/Truck_Volumes/combined_classification_counts.shp \
 $append -nln truck_counts
 
 # Truck freight functional classification crap
+echo "truck freight functional classification"
 ogr2ogr $ofmt $opath $orig/FGTSWA.gdb \
 $append -nln freight_classification
 
 # Manufacturing and Industrial Centers
+echo "manufacturing centers"
 ogr2ogr $ofmt $opath $orig/mic/micen_fixed.sqlite \
 $append -nln manufacturing_centers
