781a18fb147aa7ce3633caffb974315afd6d5785
Peter Schmiedeskamp <peter@thoughtspot.net>
2013-09-18 12:23:07 -0700

    add read_only flag for spatialite4.x

--- src/traces/traces-filter.sql
+++ src/traces/traces-filter.sql
@@ -1,5 +1,5 @@
 -- Drop traces_filter table if it already exists
-DROP VIEW traces_filter;
+DROP VIEW traces_filter_mbr;
 DROP VIEW traces_bad;
 
 -- Identify those traces:
@@ -21,5 +21,6 @@ WHERE b.mbr_lt_1024 = 1;
 
 -- Register the view containing misfit links
 INSERT INTO views_geometry_columns
-    (view_name, view_geometry, view_rowid, f_table_name, f_geometry_column)
-  VALUES ('traces_bad', 'geometry', 'rowid', 'traces', 'geometry');
+    (view_name, view_geometry, view_rowid, f_table_name,
+    f_geometry_column, read_only)
+  VALUES ('traces_bad', 'geometry', 'rowid', 'traces', 'geometry', 1);
